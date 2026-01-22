fe9398f886edba160ca3ab926357d08a4f66e994
Arijit Mondal <arijitm717@gmail.com>
2026-01-23 00:59:37 +0530

    learning about static & dynamic metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,25 @@
-export default async function ProductDetailsPage({
-  params,
-}: {
+/**
+ * We can only use one of then static/dynamic metadata
+ * And only we can set metadat inside SSR
+ */
+
+import { Metadata } from "next";
+
+interface Props {
   params: Promise<{ productId: string }>;
-}) {
+}
+
+// dynamicly set metadata for this page
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = await params;
+  return {
+    title: `Product details of ${productId}`,
+  };
+};
+
+export default async function ProductDetailsPage({ params }: Props) {
   const { productId } = await params;
 
   return (
