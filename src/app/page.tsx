117667af54fdc75c65f6772ca7cf85218e9e98f4
Arijit Mondal <arijitm717@gmail.com>
2026-01-23 11:03:21 +0530

    link component in nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <section>Wellcome To The Home Page</section>;
+  return <section>
+    <h1>Wellcome To The Home Page</h1>
+    <Link href="/products">Products</Link>
+    <Link href="/about">About</Link>
+    <Link href="/docs">Docs</Link>
+    </section>;
 }
