444e2fef22b13d40895fb495aa1ffc78b6ee6bd9
Arijit Mondal <arijitm717@gmail.com>
2026-01-23 11:52:50 +0530

    programeticaly navigate

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
-    return <section>Login page</section>
+  const router = useRouter();
+
+  const handlelogin = () => {
+    console.log("user loggedin successfully");
+    router.push("/");
+  };
+
+  return (
+    <section>
+      <h1>Login page</h1>
+      <button onClick={handlelogin}>Login</button>
+    </section>
+  );
 }
